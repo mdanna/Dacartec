bdcb8d3bdad63857ee38908db50ab6843adef531
Mario D'Anna <mario.danna@hcl.com>
2022-04-04 18:03:31 +0200

    Search Area implementation

--- controllers/desktop/frmLoginController/frmLoginController.js
+++ controllers/desktop/frmLoginController/frmLoginController.js
@@ -15,17 +15,7 @@ define({
 
   onInit(){
     
-    this.view.flxLogin.onHover = (flex, context) => {
-      if(context.eventType === constants.ONHOVER_MOUSE_LEAVE){
-        this.view.flxLogin.skin = 'sknFlxDarkBlueBorderWhite';
-        this.view.lblLogin.skin = 'sknLblWhite20';
-      } else if(context.eventType === constants.ONHOVER_MOUSE_ENTER){
-        this.view.flxLogin.skin = 'sknFlxWhiteBorderDarkBlue';
-        this.view.lblLogin.skin = 'sknLblDarkBlue20';
-      }
-    };
-    
-    this.view.flxLogin.onClick = () => {
+    this.view.loginButton.onClick = () => {
       const userid = this.view.txtUser.text;
       const password = this.view.txtPassword.text;
       mbaas.login(userid, password).then((result) => {
---
+++ userwidgets/com.dacartec.SimpleButton/modules/require/SimpleButtonController.js
@@ -0,0 +1,35 @@
+define(function() {
+  
+  const SKIN_FLEX_SELECTED = 'sknFlxWhiteBorderDarkBlue';
+  const SKIN_FLEX_UNSELECTED = 'sknFlxDarkBlueBorderWhite';
+  const SKIN_LBL_SELECTED = 'sknLblDarkBlue20';
+  const SKIN_LBL_UNSELECTED = 'sknLblWhite20';
+
+  return {
+    constructor: function(baseConfig, layoutConfig, pspConfig) {
+      this.view.preShow = () => {
+        if(!this.initDone){
+          this.view.onClick = () => this.onClick();
+
+          this.view.onHover = (flex, context) => {
+            if(context.eventType === constants.ONHOVER_MOUSE_LEAVE){
+              this.view.skin = SKIN_FLEX_UNSELECTED;
+              this.view.lblLogin.skin = SKIN_LBL_UNSELECTED;
+            } else if(context.eventType === constants.ONHOVER_MOUSE_ENTER){
+              this.view.skin = SKIN_FLEX_SELECTED;
+              this.view.lblLogin.skin = SKIN_LBL_SELECTED;
+            }
+          };
+          
+          this.initDone = true;
+        }
+      };
+    },
+    //Logic for getters/setters of custom properties
+    initGettersSetters: function() {
+
+    },
+
+    onClick(){}
+  };
+});
