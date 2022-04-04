88bd2da5d7f661120500c113d2be2ff0215989a1
Mario D'Anna <mario.danna@hcl.com>
2022-04-04 15:19:49 +0200

    After restoring forms

--- controllers/desktop/frmDataController/frmDataController.js
+++ controllers/desktop/frmDataController/frmDataController.js
@@ -33,19 +33,15 @@ define({
 
     //use this.navigationContext to access the arg passed by the Navigation object
 
-    voltmx.application.showLoadingScreen(null, "Loading...", constants.LOADING_SCREEN_POSITION_FULL_SCREEN, false, true, {});
-
     mbaas.invokeOperation(mbaas.SERVICE, 'ConsultaCultivos', {}, {
       PK_Kulturstamm:"B9C054A2-9E72-4B9E-89AC-DCC05BD7A1E2",
       Sprache:"ES-ES"
     }).then((results) => {
       //this corresponds to the resolve branch
-      voltmx.application.dismissLoadingScreen();
       alert(JSON.stringify(results));
     }).catch((error) => {
       //this corresponds to the reject;
       alert(JSON.stringify(error));
-      voltmx.application.dismissLoadingScreen();
     });
 
   },
