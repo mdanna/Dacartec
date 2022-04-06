29f505529ff1d06737163e06405243c22d2e05db
Mario D'Anna <mario.danna@hcl.com>
2022-04-06 17:00:00 +0200

    Finished first release of Cultivo implementation with pagination

---
+++ modules/globals.js
@@ -0,0 +1,6 @@
+const globals = {
+  PAGE_SIZE: 10,
+  
+  EVENT_PAGINATE: 'evt_paginate',
+  EVENT_UPDATE_PAGINATION: 'evt_update_pagination'
+};
--- userwidgets/com.dacartec.Pagination/modules/require/PaginationController.js
+++ userwidgets/com.dacartec.Pagination/modules/require/PaginationController.js
@@ -2,22 +2,56 @@ define(function() {
 
   return {
     constructor: function(baseConfig, layoutConfig, pspConfig) {
-      
-      eventManager.subscribe('loadData', ({datasetName, count, pageNumber}) => {
+
+      eventManager.subscribe(globals.EVENT_UPDATE_PAGINATION, ({datasetName, count, pageNumber}) => {
         if(datasetName === this.datasetName){
-          this.view.lblPageInfo.text = `${(this.pageSize * (pageNumber - 1)) + 1} - ${pageNumber * this.pageSize} of ${count}`; 
+          this.count = count;
+          this.pageNumber = pageNumber;
+          this.view.isVisible = count > 0;
+          this.view.lblPageInfo.text = `${(globals.PAGE_SIZE * (pageNumber - 1)) + 1} - ${Math.min(count, pageNumber * globals.PAGE_SIZE)} of ${count}`; 
         }
       });
+
+      this.view.preShow = () => {
+
+        if(!this.initDone){
+
+          this.view.flxGoTop.onClick = () => {
+            eventManager.publish(globals.EVENT_PAGINATE, {
+              datasetName: this.datasetName,
+              pageNumber: 1
+            });
+          };
+
+          this.view.flxGoUp.onClick = () => {
+            eventManager.publish(globals.EVENT_PAGINATE, {
+              datasetName: this.datasetName,
+              pageNumber: Math.max(1, this.pageNumber - 1)
+            });
+          };
+
+          this.view.flxGoDown.onClick = () => {
+            eventManager.publish(globals.EVENT_PAGINATE, {
+              datasetName: this.datasetName,
+              pageNumber: Math.min(Math.ceil(this.count/globals.PAGE_SIZE), this.pageNumber + 1)
+            });
+          };
+
+          this.view.flxGoBottom.onClick = () => {
+            eventManager.publish(globals.EVENT_PAGINATE, {
+              datasetName: this.datasetName,
+              pageNumber: Math.ceil(this.count/globals.PAGE_SIZE)
+            });
+          };
+
+          this.initDone = true;
+        }
+
+      };
 
     },
     //Logic for getters/setters of custom properties
     initGettersSetters: function() {
-      defineGetter(this, 'pageSize', () => {
-        return this._pageSize;
-      });
-      defineSetter(this, 'pageSize', value => {
-        this._pageSize = value;
-      });
       defineGetter(this, 'pageNumber', () => {
         return this._pageNumber;
       });
