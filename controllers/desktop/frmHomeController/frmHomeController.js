c7de5c79175a1e8d07b6f1ef460ef4c1c2fba186
Mario D'Anna <mario.danna@hcl.com>
2022-04-08 11:33:03 +0200

    Hamburger menu implementation

--- controllers/desktop/frmHomeController/frmHomeController.js
+++ controllers/desktop/frmHomeController/frmHomeController.js
@@ -8,7 +8,12 @@ define({
 
   onInit(){
     this.view.homePageHeader.onClickLeft = () => this.view.cmpHamburgerMenu.toggle(true);
+    
 	this.view.flxCultivo.onClick = () => new voltmx.mvc.Navigation('frmCultivo').navigate();
+    
+    this.view.cmpHamburgerMenu.onItemSelected = (key) => {
+      alert(key);
+    };
   }
 
 });
--- userwidgets/com.dacartec.HamburgerMenu/modules/require/HamburgerMenuController.js
+++ userwidgets/com.dacartec.HamburgerMenu/modules/require/HamburgerMenuController.js
@@ -1,4 +1,5 @@
 define(function() {
+  const MENU_LEFT = -300;
 
   return {
     constructor: function(baseConfig, layoutConfig, pspConfig) {
@@ -11,26 +12,29 @@ define(function() {
           this.view.menuHeader.onClickLeft = () => {
             this.toggle(false);
           };
+          
+          this.view.segMenuItems.onRowClick = () => {
+            this.toggle(false);
+            this.onItemSelected(this.view.segMenuItems.selectedRowItems[0].key);
+          };
 
           this.initDone = true;
         }
       };
 
     },
-    //Logic for getters/setters of custom properties
-    initGettersSetters: function() {
-
-    },
+    
+    initGettersSetters() {},
 
     toggle(open){
       const self = this;
       open && (self.view.isVisible = true);
       this.view.flxMenu.animate(voltmx.ui.createAnimation({
         "0": {
-          left: open ? -200 : 0
+          left: open ? MENU_LEFT : 0
         },
         "100": {
-          left: open ? 0 : -200
+          left: open ? 0 : MENU_LEFT
         }
       }), {
         "duration": 0.5,
@@ -38,12 +42,16 @@ define(function() {
         "delay": 0,
         "fillMode": kony.anim.FILL_MODE_FORWARDS
       }, {
-        animationStart: function() {
-        },
+        animationStart: function() {},
         animationEnd: function() {
           open || (self.view.isVisible = false);
         }
       });
+    },
+    
+    onItemSelected(key){
+      voltmx.print(`Selected menu item ${key}`);
     }
+    
   };
 });
